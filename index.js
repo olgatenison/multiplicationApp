50fecb00ba96815293bf188b8aceb8f9fb6b600a
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-08-10 22:31:38 +0300

    add marks and functions

---
+++ index.js
@@ -0,0 +1,36 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+  score = 0;
+}
+
+questionEl.innerText = `What is ${num1}
+multiply by ${num2}?`;
+
+const correctAnswer = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAnswer = +inputEl.value;
+  console.log(userAnswer);
+  if (userAnswer === correctAnswer) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
+  console.log(score);
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
+
+scoreEl.innerText = `score: ${score}`;
